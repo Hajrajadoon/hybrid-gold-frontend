16384b7b986fab53a0d8d2ebc63bfd2b0524a3f2
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-08-23 14:04:49 +0500

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,19 @@
 import React from "react";
 
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => <input {...props} />;
+interface InputProps {
+  type?: string;
+  value: number | string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  min?: number;
+  className?: string;
+}
+
+export const Input: React.FC<InputProps> = ({ type = "text", value, onChange, min, className }) => (
+  <input
+    type={type}
+    value={value}
+    min={min}
+    onChange={onChange}
+    className={`border rounded px-2 py-1 ${className}`}
+  />
+);
